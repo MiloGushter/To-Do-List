be642b197696074b8181503d1f0adee573b5aedc
Milo <milojevic.luka2002@gmail.com>
2022-12-24 20:49:27 +0100

    Added html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,41 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { plugins } = require("../Restaurant-landing-page/webpack.config");
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  devtool: "inline-source-map",
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: "img/[hash][name][ext]",
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
-        type: "asset/resource",
-        generator: {
-          filename: "img/[hash][name][ext]",
-        },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      tilte: "To-do list",
-      filename: "index.html",
-    }),
-  ],
+	entry: "./src/index.js",
+	mode: "development",
+	devtool: "inline-source-map",
+	output: {
+		filename: "[name].js",
+		path: path.resolve(__dirname, "dist"),
+		assetModuleFilename: "resources/[hash][name][ext]",
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/,
+				type: "asset/resource",
+				generator: {
+					filename: "resources/[name]-[hash][ext]",
+				},
+			},
+			{
+				test: /\.html$/i,
+				use: "html-loader",
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			tilte: "To-do-list",
+			template: "./src/index.html",
+			favicon: "./src/resources/icons/favicon.ico",
+			filename: "index.html",
+		}),
+	],
 };
