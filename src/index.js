25d24984a975425afbf1f86c4f73c4ebe19483ac
Milo <milojevic.luka2002@gmail.com>
2022-12-25 02:15:26 +0100

    Finished nav and sidebar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import "./sass/main.scss";
 import { openHamburgerMenu } from "./modules/hamburgerMenu.js";
+import { changeIcons } from "./modules/changeIconFill";
+import { openSidebar } from "./modules/sidebarOpen";
 openHamburgerMenu();
+changeIcons();
+openSidebar();
