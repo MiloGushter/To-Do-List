68ddaff97a7a337b5c0f0ea541869839177a2ee2
Milo <milojevic.luka2002@gmail.com>
2022-12-26 16:38:02 +0100

    Addded functionality for adding projects and started modal for tasks/to-dos

---
+++ src/modules/projectCreation.js
@@ -0,0 +1,4 @@
+export function projectCreation(projectName, projectTasks) {
+	this.projectName = projectName;
+	this.projectTasks = projectTasks;
+}
--- src/modules/projectModal.js
+++ src/modules/projectModal.js
@@ -1,39 +1,44 @@
+import { projectCreation } from "./projectCreation";
 export function showProjectModal() {
-	const modal = document.querySelector(".modal");
-	const saveButton = document.querySelector(".save-button");
-	const cancelButton = document.querySelector(".cancel-button");
+	const modal = document.querySelector("#modal-project");
+	const saveButton = document.querySelector("#save-button-project");
+	const cancelButton = document.querySelector("#cancel-button-project");
+	const projectName = document.querySelector("#input-project-name");
 	const createProject = document.querySelector(".add-projects");
+	let projects = [];
 
-	saveButton.onclick = () => {
-		const projectName = document.querySelector("#inputField");
-		console.log(`Name of the project is ${projectName.value}`);
-
+	function addStyles() {
+		projectName.value = "";
 		modal.classList.add("modal-going-out");
 		setTimeout(() => {
 			modal.classList.remove("modal-going-out");
 			modal.style.display = "none";
 		}, 300);
+	}
+
+	saveButton.onclick = () => {
+		console.log(`Name of the project is ${projectName.value}`);
+		projects.push(new projectCreation(projectName.value, {}));
+		console.log(projects);
+
+		const projectsList = document.querySelector(".side-projects-list");
+		const projectsListItem = document.createElement("li");
+		projectsListItem.classList.add("side-projects-list-item");
+		projectsListItem.textContent = projectName.value;
+		projectsList.append(projectsListItem);
+
+		addStyles();
 	};
 
 	createProject.onclick = () => {
 		modal.style.display = "block";
 	};
 
-	cancelButton.onclick = () => {
-		modal.classList.add("modal-going-out");
-		setTimeout(() => {
-			modal.classList.remove("modal-going-out");
-			modal.style.display = "none";
-		}, 300);
-	};
+	cancelButton.onclick = () => addStyles();
 
 	window.onclick = function (event) {
 		if (event.target == modal) {
-			modal.classList.add("modal-going-out");
-			setTimeout(() => {
-				modal.classList.remove("modal-going-out");
-				modal.style.display = "none";
-			}, 300);
+			addStyles();
 		}
 	};
 }
