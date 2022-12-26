59a1d72a9448d88bb65dc298fbfd63445dddbe19
Milo <milojevic.luka2002@gmail.com>
2022-12-26 18:20:35 +0100

    Started creating sample tasks

---
+++ src/modules/taskModal.js
@@ -0,0 +1,6 @@
+export function showTaskModal() {
+	const modal = document.querySelector("#modal-task");
+	const saveButton = document.querySelector("#save-button-task");
+	const cancelButton = document.querySelector("#cancel-button-task");
+	const createTask = document.querySelector("#nav-create-task");
+}
