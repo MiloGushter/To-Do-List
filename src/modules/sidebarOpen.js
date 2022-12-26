967a2c2e3987ce5122bda0a6899bdcb9482f8603
Milo <milojevic.luka2002@gmail.com>
2022-12-26 20:05:06 +0100

    Changed the way sidebar and main shrink and expand

--- src/modules/sidebarOpen.js
+++ src/modules/sidebarOpen.js
@@ -1,7 +1,8 @@
 export function openSidebar() {
 	const toggleButton = document.querySelector(".hamburger");
 	const sidebar = document.querySelector("aside");
+	const main = document.querySelector("main");
 	toggleButton.addEventListener("click", () => {
-		sidebar.classList.toggle("open");
+		sidebar.classList.toggle("closed");
 	});
 }
