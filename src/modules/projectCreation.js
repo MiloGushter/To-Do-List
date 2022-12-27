ad892bf242c1012b6accef42d6dcca13a5331cc2
Milo <milojevic.luka2002@gmail.com>
2022-12-27 12:22:02 +0100

    Renamed factory function

--- src/modules/projectCreation.js
+++ src/modules/projectCreation.js
@@ -1,4 +1,4 @@
-export function projectCreation(projectName, projectTasks) {
+export function createProject(projectName, projectTasks) {
 	this.projectName = projectName;
 	this.projectTasks = projectTasks;
 }
