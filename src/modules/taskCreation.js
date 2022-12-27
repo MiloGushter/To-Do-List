4f459c3a9c2d801117d8c65a95235e2b00a3b584
Milo <milojevic.luka2002@gmail.com>
2022-12-27 23:20:09 +0100

    Rewrote factory function

--- src/modules/taskCreation.js
+++ src/modules/taskCreation.js
@@ -1,7 +1,7 @@
-export const createTask = (name, description, date, fromProject, priority) => {
+export function createTask(name, description, date, fromProject, priority) {
 	this.name = name;
 	this.description = description;
 	this.date = date;
 	this.fromProject = fromProject;
 	this.priority = priority;
-};
+}
