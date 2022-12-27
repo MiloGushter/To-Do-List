c1b5436c281d189662a84a53f9a3eac893936e13
Milo <milojevic.luka2002@gmail.com>
2022-12-27 12:22:47 +0100

    Created factory function for creating tasks

---
+++ src/modules/taskCreation.js
@@ -0,0 +1,13 @@
+export const createTask = (
+	taskName,
+	taskDescription,
+	taskDate,
+	taskOfProject,
+	taskPriority
+) => {
+	this.taskName = taskName;
+	this.taskDescription = taskDescription;
+	this.taskDate = taskDate;
+	this.taskOfProject = taskOfProject;
+	this.taskPriority = taskPriority;
+};
