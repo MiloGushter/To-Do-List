acc542b901085325f8ab0638c6e1213c8f085d5d
Milo <milojevic.luka2002@gmail.com>
2022-12-31 11:35:03 +0100

    Removed main as it is unnecessary

--- src/modules/toggleSidebar.js
+++ src/modules/toggleSidebar.js
@@ -1,7 +1,6 @@
 export function openSidebar() {
 	const toggleButton = document.querySelector(".hamburger");
 	const sidebar = document.querySelector("aside");
-	const main = document.querySelector("main");
 	toggleButton.addEventListener("click", () => {
 		sidebar.classList.toggle("closed");
 	});
