b4252d6b6db97f0f74b53a47e08eada49c67deda
Milo <milojevic.luka2002@gmail.com>
2023-01-02 00:33:18 +0100

    Imported new functions

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import "./sass/main.scss";
 import toggles from "./modules/toggles";
 import { changeIcons } from "./modules/changeIconFill";
 import { showProjectModal } from "./modules/projectModal";
-import { showTaskModal, populateProjectsForTask } from "./modules/taskModal";
-import { onLoadPopulate } from "./modules/populateMainContent";
-import { populateProjectsList } from "./modules/projectModal";
+import { showTaskModal } from "./modules/taskModal";
+import {
+	onLoadPopulate,
+	populateProjectsForTaskOnLoad,
+	populateProjectsListOnLoad,
+} from "./modules/populateMainContent";
 toggles();
 changeIcons();
 showProjectModal();
 showTaskModal();
 onLoadPopulate();
-populateProjectsList();
-populateProjectsForTask();
+populateProjectsListOnLoad();
+populateProjectsForTaskOnLoad();
