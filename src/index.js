5a781217a7092560a38b109b3ccf3c3b4f7f7c6a
Milo <milojevic.luka2002@gmail.com>
2023-01-03 23:24:13 +0100

    Changed call order of functions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import "./sass/main.scss";
 import toggles from "./modules/toggles";
 import { changeIcons } from "./modules/changeIconFill";
-import { showTaskModal, showProjectModal } from "./modules/showModals";
+import { showTaskModal, showProjectModal, showTaskEditModal } from "./modules/showModals";
 import {
 	onLoadPopulate,
 	populateProjectsForTaskOnLoad,
 	populateProjectsListOnLoad,
 } from "./modules/populateMainContent";
 toggles();
-changeIcons();
-showProjectModal();
-showTaskModal();
 onLoadPopulate();
 populateProjectsListOnLoad();
 populateProjectsForTaskOnLoad();
+changeIcons();
+showProjectModal();
+showTaskModal();
